2558c70c028715efad11155723112ba5a7401bc8
Sivarajesh B <b.sivarajesh20@gmail.com>
2022-08-26 00:09:18 +0530

    Hibernata and JPA notes added, RestAPI notes DS and ALgo started

---
+++ DS_ALGO/Algo_Sorting/src/com/siva/algorithim/Main.java
@@ -0,0 +1,11 @@
+package com.siva.algorithim;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Sorting sort = new Sorting(new int[] {1,5,6,8,0});
+		sort.bubbleSort();
+		sort.printSortedArray();
+	}
+
+}
