51a3aa4aa9aa152aae2d5020c2c86dc55e1a1187
1938639 <b.sivarajesh@tcs.com>
2023-07-14 18:33:52 +0530

    Expense project is updated with components

---
+++ React/expense-tracker/src/component/Expense/Expenses.js
@@ -0,0 +1,13 @@
+import Card from '../UI/Card';
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+export default function Expenses({expenses}){
+    return(
+        <Card className="expenses">
+        <ExpenseItem expense = {expenses[0]} />
+        <ExpenseItem expense = {expenses[1]} />
+        <ExpenseItem expense = {expenses[2]} />
+        <ExpenseItem expense = {expenses[3]} />
+        </Card>
+    );
+}
